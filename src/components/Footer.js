7761473cfea9c4120b66a438bf74f7e1dcf68850
onethejay <jhjang951@gmail.com>
2022-04-30 00:42:42 +0900

    레이아웃 분할 준비

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Footer() {
+    return <footer>안녕하세요 푸터</footer>
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Header() {
+    return <header>안녕하세요 헤더</header>
+}
+
+export default Header;
